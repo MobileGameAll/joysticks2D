70a3f9b82aab387bf4ac8ef079b4a06cf1e23c0a
Francisco Calaça Xavier <chicocx@gmail.com>
2017-02-06 10:46:15 -0200

    First commit

---
+++ Assets/Joystick2DController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Joystick2DController : MonoBehaviour {
+
+    private Rigidbody2D body;
+
+    public float moveForce = 5;
+
+	// Use this for initialization
+	void Start () {
+        this.body = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") * moveForce, CrossPlatformInputManager.GetAxis("Vertical") * moveForce);
+        body.AddForce(moveVec);
+	}
+}
